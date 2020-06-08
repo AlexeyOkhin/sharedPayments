d465263c25a75577d87ce31b5b842041a674351a
Alexey <>
2020-06-08 23:30:54 +0500

    add controllers

---
+++ sharePayments/addEventViewController.swift
@@ -0,0 +1,22 @@
+//
+//  addEventViewController.swift
+//  sharePayments
+//
+//  Created by Alexey on 08.06.2020.
+//  Copyright © 2020 Alexey Okhin. All rights reserved.
+//
+
+import UIKit
+
+class addEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ sharePayments/addMemberViewController.swift
@@ -0,0 +1,20 @@
+//
+//  addMemberViewController.swift
+//  sharePayments
+//
+//  Created by Alexey on 08.06.2020.
+//  Copyright © 2020 Alexey Okhin. All rights reserved.
+//
+
+import UIKit
+
+class addMemberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+
+}
