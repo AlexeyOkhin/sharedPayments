4192e5f2ef1d6860d9c7a2f44fdae6b15c67030f
Alexey <>
2020-06-26 20:01:39 +0500

    rename classes

---
+++ sharePayments/Controller/controller members/AddMemberViewController.temp_caseinsensitive_rename.swift
@@ -0,0 +1,48 @@
+//
+//  addMemberViewController.swift
+//  sharePayments
+//
+//  Created by Alexey on 08.06.2020.
+//  Copyright © 2020 Alexey Okhin. All rights reserved.
+//
+
+import UIKit
+
+class AddMemberViewController: UIViewController {
+    
+    @IBOutlet weak var nameMemberTextField: UITextField!
+    @IBOutlet weak var payMemberTextField: UITextField!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
+    var member = Member(name: "", payment: 0)
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameMemberTextField.text = member.name
+        payMemberTextField.text = String(member.payment)
+        doneButton.isEnabled = false
+
+        
+    }
+    
+    @IBAction func nameMemberAction(_ sender: UITextField) {
+        
+        if (sender.text?.isEmpty)! {
+            doneButton.isEnabled = false
+        } else {
+            doneButton.isEnabled = true
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "unwindDone" else { return }
+        let name = nameMemberTextField.text ?? ""
+        let pay = Int(payMemberTextField.text!)! 
+        
+        self.member = Member(name: name, payment: pay)
+    }
+   
+
+}
